4d07c4867c6426a151341aecc44bc712ead44614
Chandra Amarasingham <camarasingham@yahoo.com>
2018-10-13 10:54:33 +0000

    fixing faulty import

--- meet/views.py
+++ meet/views.py
@@ -9,7 +9,6 @@ from django.db import transaction
 import json
 import datetime
 from django.utils import timezone
-from mygenerateToken import get_token
 import subprocess 
 
 HOST_MEET_TIMECHECK = 3600
